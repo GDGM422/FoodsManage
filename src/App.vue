9db350fe457f0c67a2c7edc679b89c201f07e576
unknown <13169484031@163.com>
2017-08-11 13:39:15 +0800

    wan

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-<<<<<<< HEAD
   color: #2c3e50;
-=======
->>>>>>> 2763b416fa5b7fab225641aefded7ca1f0a66864
 }
-
 </style>
